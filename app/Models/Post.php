2fb2956f0f869d91fe905994b7fbf75a0dcc26a2
vldsy <vldsyata@gmail.com>
2024-10-30 15:23:09 +0100

    added post factory and seeder to get the default model

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -5,8 +5,10 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 use Spatie\MediaLibrary\HasMedia;
 use Spatie\MediaLibrary\InteractsWithMedia;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Post extends Model implements HasMedia
 {
     use InteractsWithMedia;
+    use HasFactory;
 }
