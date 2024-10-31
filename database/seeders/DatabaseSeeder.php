a949d77a571fef79bc3f553c70a819d76c8ebc56
vldsy <vldsyata@gmail.com>
2024-10-31 15:16:45 +0100

    created 2 users to test file upload with auth user

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
 
 class DatabaseSeeder extends Seeder
 {
@@ -15,6 +16,28 @@ class DatabaseSeeder extends Seeder
     {
         $this->call([
             PostSeeder::class,
+        ]);
+
+        // Create 2 users
+        //User::truncate();
+        User::where('id', '>', 0)->delete();
+
+        $password = "12345";
+
+        User::create([
+            'name' => "John Doe",
+            'email' => "johndoe@gmail.com",
+            'email_verified_at' => now(),
+            'password' => bcrypt($password),
+            'remember_token' => Str::random(10),
+        ]);
+
+        User::create([
+            'name' => "Bill Gates",
+            'email' => "billgates@gmail.com",
+            'email_verified_at' => now(),
+            'password' => bcrypt($password),
+            'remember_token' => Str::random(10),
         ]);
     }
 }
